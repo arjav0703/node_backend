eb0dc51cac68221ce0612acd11b86b11cd99c3a4
Arjav Jain <arjavjain0703@gmail.com>
2025-04-02 21:02:58 +0530

    fuck my internet

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+
+app.use((req, res, next) => {});
+
 app.get('/', (req, res) => {
   res.send(`Hello, World!, you have requested for ${req.url}`);
 });
