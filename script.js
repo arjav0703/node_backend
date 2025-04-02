cfe9616d21ea4997abfdc29d9c3f6a39b0c253fd
Arjav Jain <arjavjain0703@gmail.com>
2025-04-02 21:25:26 +0530

    welcome to matix

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 
-
-app.use((req, res, next) => {});
+// Middleware
+app.use((req, res, next) => {
+    next();
+});
 
 app.get('/', (req, res) => {
   res.send(`Hello, World!, you have requested for ${req.url}`);
 });
+
+app.get(
+    '/showmatrixdata/:humanidentifier', (req, res) => {
+        const hm = req.params.humanidentifier;
+        res.send(`hey matrix admin, please verify your identity to get the details of ${hm}`)
+    }
+)
 
 app.listen(3000)
